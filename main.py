5572e301bdead213c100c8e3acefe888e6005b08
desamurr <1alinurrra@gmail.com>
2025-09-28 19:07:39 +0500

    Шаг 2: обработка ошибок и цикл повторных операций

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-num1 = float(input("Введите первое число: "))
-num2 = float(input("Введите второе число: "))
-op = input("Введите операцию (+, -, , /): ")
+while True:
+    try:
+        num1 = float(input("Введите первое число: "))
+        num2 = float(input("Введите второе число: "))
 
-if op == "+":
-    print(num1 + num2)
-elif op == "-":
-    print(num1 - num2)
-elif op == "":
-    print(num1 * num2)
-elif op == "/":
-    print(num1 / num2)
-else:
-    print("Неизвестная операция")
+        op = input("Введите операцию (+, -, , /) или 0 для выхода: ")
+
+        if op == "0":
+            print("Выход из программы...")
+            break
+        elif op == "+":
+            print(num1 + num2)
+        elif op == "-":
+            print(num1 - num2)
+        elif op == "":
+            print(num1 * num2)
+        elif op == "/":
+            if num2 == 0:
+                print("Ошибка: деление на ноль невозможно!")
+            else:
+                print(num1 / num2)
+        else:
+            print("Неизвестная операция")
+
+    except ValueError:
+        print("Ошибка: введите корректное число!")
+
+    print("\n---\n")
